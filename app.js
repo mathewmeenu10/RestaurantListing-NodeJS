16189fb1dabfacfe4e5a819125a7cdf6f07f860a
mathewmeenu10 <mathewmeenu10@gmail.com>
2024-04-06 09:15:23 -0400

    view template

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 require("dotenv").config();
+const path = require('node:path');
 const express = require("express");
 const mongoose = require("mongoose");
 const appConfig = require("./package.json");
 
+const { engine } = require('express-handlebars');
 const app = express();
 const { body, validationResult } = require('express-validator');
 const database = require("./config/database");
 const bodyParser = require("body-parser"); // pull information from HTML POST (express4)
 
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    runtimeOptions:{allowProtoPropertiesByDefault:true,
+      allowedProtoMethodsByDefault:true},
+    helpers: {},
+}));
+app.set('view engine', '.hbs');
+
 const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
+
+const Handlebars = require('handlebars');
 
 const Restaurant = require("./models/restaurant");
 
@@ -23,16 +36,64 @@ Restaurant.initialize(database.url).then(()=>{
     console.log("Error Initializing in mongodb and server",err);
 })
 
-app.get('/api/restaurants', [
-    body('page').isNumeric().notEmpty(),
-    body('perPage').isNumeric().notEmpty(),
-    body('borough').optional().isString(),
-], async (req, res) => {
+app.get('/api/list_all_restaurants', (req, res) => {
+    res.render('search_restaurants', { 
+        title: "Search Restaurants",
+    });
+});
+
+app.post('/api/list_all_restaurants', async (req, res) => {
+
+    let page = req.body.page;
+    let perPage = req.body.borough;
+    let borough = req.body.borough || '';
+
+    const validationErrors = validationResult({
+        query: { page, perPage, borough }
+    });
+
+    if (!validationErrors.isEmpty()) {
+        throw { status: 400, message: 'Validation failed', errors: validationErrors.array() };
+    }
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+        // return res.status(400).json({ errors: errors.array() });
         return res.status(400).json({ message: 'Page  not found' });
     }
-    const borough = req.query.borough || '';
+    // borough = req.query.borough || '';
+
+    try {
+        const restaurants = await Restaurant.getAllRestaurants(req.body.page, req.body.perPage, borough);
+        res.render('index_restaurants', { 
+            title: "Restaurants",
+            restaurants
+        });
+        // res.json(restaurants);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Page  not found' });
+    }
+});
+
+app.get('/api/restaurants', async (req, res) => {
+
+    let page = req.query.page;
+    let perPage = req.query.borough;
+    let borough = req.query.borough || '';
+
+    const validationErrors = validationResult({
+        query: { page, perPage, borough }
+    });
+
+    if (!validationErrors.isEmpty()) {
+        throw { status: 400, message: 'Validation failed', errors: validationErrors.array() };
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ message: 'Page  not found' });
+    }
+    // borough = req.query.borough || '';
 
     try {
         const restaurants = await Restaurant.getAllRestaurants(req.query.page, req.query.perPage, borough);
